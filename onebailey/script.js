9a766596128d5b370c16eb07c45a5df6836e1b95
Jongwook Park <nedpark@outlook.com>
2023-04-27 11:16:32 +0900

    구글 분석기 적용

--- onebailey/script.js
+++ onebailey/script.js
@@ -3,23 +3,23 @@ function openLink(index) {
 
     switch(index) {
         case 0:
-            link = "https://docs.google.com/spreadsheets/d/1UqsJSAqmz3_kKMMjGmNItDxKR8zV_ULH2uDtx7myK_8"
+            link = "./move_0.html"
             break
 
         case 1:
-            link = "https://docs.google.com/forms/d/1VvuarwIhKICcKLZ_2hYtXXMpuMhad-dk957Hs0bwCU4"
+            link = "./move_1.html"
             break
 
         case 2:
-            link = "https://docs.google.com/forms/d/1hG3-vTCF13BmS2P4HxkOavtEYazB724l1mfGa4GuLbE"
+            link = "./move_2.html"
             break
 
         case 3:
-            link = "https://docs.google.com/forms/d/1rsAwnFttW6CNN6DIGCOhLio4Q4TiZs9WUWAA4W76hbI"
+            link = "./move_3.html"
             break
 
         case 4:
-            link = "https://docs.google.com/forms/d/1aMLKaytD0TTBSdlltYsE9SLUaQjiLlusVtFXMzQpIWQ"
+            link = "./move_4.html"
             break
     }
     
